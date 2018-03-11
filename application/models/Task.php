8cbbac7a5b8ee8c69f9bc0320f9e53b3873b7add
Sheldon Lynn <sheldonlynn@outlook.com>
2018-03-11 14:29:03 -0700

    Completed phpunit tests

--- application/models/Task.php
+++ application/models/Task.php
@@ -1,40 +1,48 @@
 <?php
 
 class Task extends Entity {
-    private $dateofbirth;
-    private $age;
-    private $name;
+    public $id;
+    public $priority;
+    public $task;
+    public $size;
+    public $group;
 
-    public function setName($value) {
-        if (empty($value)) {
-            throw new Exception("Name can't be empty");
+    public function setId($value) {
+        if (empty($value))
+            throw new Exception('ID ust exist');
+        $this->id = $value;
+    }
+
+    public function setPriority($value) {
+        if (! is_numeric($value))
+            throw new Exception('Must contain priority');
+        if ($value > 4)
+            throw new Exception('Priority must be < 4');
+        $this->priority = $value;
+    }
+
+    public function setTask($value) {
+        if (strlen($value) > 64) {
+            throw new Exception('Task cannot be longer than 64 chars');
         }
-        if (strlen(value) > 30) {
-            throw new Exception('A Name cannot be longer than 30 characters');
-        }
+        if (!ctype_alnum(trim(str_replace(' ','',$value))))
+            throw new Exception('Task is invalid');
+        $this->priority = $value;
     }
 
-    public function setDateofbirth($value) {
-        if (empty($value)) {
-            throw new Exception("Dateofbirth can't be empty");
-        }
+    public function setSize($value) {
+        if (!is_numeric($value))
+            throw new Exception('Size cannot be empty');
+        if ($value > 4)
+            throw new Exception('Please choose proper size');
+        $this->size = $value;
     }
 
-    public function setAge($value) {
-        if (empty($value)) {
-            throw new Exception("Age can't be empty");
-        }
-    }
-
-    public function getDateofbirth() {
-        return $this->dateofbirth;
-    }
-
-    public function getAge() {
-        return $this->age;
-    }
-
-    public function getName(){
-        return $this->name;
+    public function setGroup($value) {
+        if (!is_numeric($value))
+            throw new Exception('Group cannot be empty');
+        if ($value > 4)
+            throw new Exception('Please choose a proper group');
+        $this->group = $value;
     }
 }
